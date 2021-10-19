74d75a11a8c4b5f252a54fa9199ac85db92875c0
Mike Killewald <mkillewald@yahoo.com>
2021-10-18 21:51:04 -0400

    Initial Commit

---
+++ QMKagent/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  QMKagent
+//
+//  Created by k1ds3ns4t10n on 10/18/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ QMKagent/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QMKagent
+//
+//  Created by k1ds3ns4t10n on 10/18/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
